16ccd6c3d97c789162f2dd3262ed48ec4ea68860
Sebastian Vasquez <svasvil0211@gmail.com>
2025-08-11 23:29:24 -0600

    Delete ProyectoGrupo3 directory

--- ProyectoGrupo3/src/main/java/Proyecto/Proyecto/dao/RolDao.java
+++
@@ -1,8 +0,0 @@
-package Proyecto.Proyecto.dao;
-
-import Proyecto.Proyecto.domain.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolDao extends JpaRepository<Rol, Long> {
-
-}
